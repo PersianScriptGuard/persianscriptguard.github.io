992a4ec43f9d32610b10aab3d7e25b575d11be09
PersianScriptGuard <persian.script.guard@gmail.com>
2018-04-18 20:19:59 +0430

    init

---
+++ js/analytics.js
@@ -0,0 +1,17 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+      var ga = document.createElement('script');
+      ga.type = 'text/javascript';
+      ga.async = true;
+      ga.src = 'https://www.googletagmanager.com/gtag/js?id=UA-51502368-5';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(ga, s);
+
+$(document).ready(function(){
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-51502368-5');
+});
